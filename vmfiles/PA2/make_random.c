50f5b6c6fc465ec00f283eb55e919f15b9097251
James McCauley <jmccauley@mtholyoke.edu>
2020-10-28 13:47:20 -0700

    PA2: Fix typo in a comment

--- vmfiles/PA2/make_random.c
+++ vmfiles/PA2/make_random.c
@@ -2,6 +2,6 @@ int main (int argc, char * argv[])
 {
   //TODO: Check number of arguments
   //TODO: Seed random number generator
-  //TODO: Generated random numbers
+  //TODO: Generate random numbers
   return 1;
 }
