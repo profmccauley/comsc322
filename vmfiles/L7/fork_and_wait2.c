4420825d8858b6ce1955209f304319ea02a8c29e
James McCauley <jmccauley@mtholyoke.edu>
2020-10-30 00:52:09 -0700

    L7: Updating fork_and_wait

---
+++ vmfiles/L7/fork_and_wait2.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main (int argc, char * argv[])
+{
+  if ( fork() == 0 )
+  {
+    // I must be the child
+    int number = atoi(argv[1]);
+
+    printf("CHILD: Waiting five seconds...\n");
+    sleep(5);
+    printf("CHILD: Returning %i\n", number);
+
+    return number;
+  }
+
+  // I must be the parent
+  printf("PARENT: I'm waiting for my child.\n");
+
+  int status;
+  wait(&status);
+  printf("PARENT: Child exited with return value %i\n",
+         WEXITSTATUS(status));
+
+  return 0;
+}
