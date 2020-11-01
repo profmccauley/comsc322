402f8428451be8cee607db2ee86ecc79fa264ca1
James McCauley <jmccauley@mtholyoke.edu>
2020-11-01 00:47:37 -0700

    HW1: Add note about switching directories

--- vmfiles/HW1/setup.sh
+++ vmfiles/HW1/setup.sh
@@ -8,6 +8,11 @@ fi
 
 if command -v tcsh &> /dev/null ; then
   echo "You should be good to go for HW1."
+  if [[ "$PWD" != "/home/vagrant/shared" ]]; then
+    echo
+    echo "Before you get started, switch to your shared directory with:"
+    echo "  cd ~/shared"
+  fi
 else
   echo "There seems to have been a problem."
 fi
