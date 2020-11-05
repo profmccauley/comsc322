0731a9994745b05647cfe8adbc87a3033bcad314
James McCauley <jmccauley@mtholyoke.edu>
2020-11-05 02:28:26 -0700

    RQ4: Add

---
+++ vmfiles/RQ4/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(uname)" != "Linux" ]; then
+  echo "Not on Linux!"
+  echo "Maybe you're not in your VM?"
+  exit
+fi
+
+# dir name from stackoverflow 59895
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+NAME="$(basename $DIR)"
+SF=~/shared
+
+if [ ! -e $SF ]; then
+  echo "Shared folder not found."
+  exit
+fi
+if [ -e $SF/$NAME ]; then
+  echo "It looks like $NAME was already set up."
+  exit
+fi
+
+cp -nPR $DIR $SF/$NAME
+cd $SF/$NAME
+
+echo "Downloading..."
+HW=HW-MLFQ
+NM=mlfq
+wget http://pages.cs.wisc.edu/~remzi/OSTEP/Homework/$HW.tgz
+
+echo "Decompressing..."
+tar xf $HW.tgz
+
+echo "Patching it so it actually works..."
+sed -i.original '0,/python/s//python2/' $NM.py
+
+echo "Removing strange print statement (?!)..."
+sed -i.original2 "0,/\(^        print 'IO DONE'\$\)/s//#\1/" $NM.py
+
+echo "Renaming the README for Windows users..."
+mv README-$NM README-$NM.txt
+
+echo
+echo "*** $NAME should now be ready in $SF/$NAME ***"
